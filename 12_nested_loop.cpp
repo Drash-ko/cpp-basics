3ee3156881262ede614a93aabc64ddb96d71446e
Drashko <n.drashko@gmail.com>
2025-11-12 17:59:50 +0100

    Add example of nested for loop

---
+++ 12_nested_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // пример вложенного цикла for
+  for (int a = 1; a <= 5; a++) {
+    cout << "First loop № " << a << endl;
+
+    for (int b = 1; b <= 3; b++) {
+      cout << "\tSecond loop № " << b << endl;
+    }
+  }
+}
